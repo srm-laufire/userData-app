15bed95263ff3c2467145ef8aff01fde9203b0e0
srm-laufire <srm@laufire.in>
2021-09-27 09:20:55 +0000

    Refactored some styles.

--- src/components/user.js
+++ src/components/user.js
@@ -6,9 +6,9 @@ const User = (user) => {
 	const { id, name, age, gender } = user;
 
 	return <div key={ id } role="user">
-		<span role="name" className="user-style">{ name }</span>
-		<span role="age" className="user-style">{ age }</span>
-		<span role="gender" className="user-style">{ gender }</span>
+		<span role="name" className="name">{ name }</span>
+		<span role="age" className="age">{ age }</span>
+		<span role="gender" className="gender">{ gender }</span>
 		<span>{ RemoveButton(user) }</span>
 	</div>;
 };
