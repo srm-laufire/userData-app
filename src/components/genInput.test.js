032a6dcb48e368d51018c6428159dc35dbc8e31d
srm-laufire <srm@laufire.in>
2021-09-29 04:18:46 +0000

    Tested onChange for genInput.js

--- src/components/genInput.test.js
+++ src/components/genInput.test.js
@@ -1,10 +1,10 @@
+/* eslint-disable max-lines-per-function */
 import { getRndString } from '../helpers';
 import { rndBetween } from '@laufire/utils/random';
-import { render } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 import genInput from './genInput';
 import { secure } from '@laufire/utils/collection';
 
-// eslint-disable-next-line max-lines-per-function
 describe('genInput', () => {
 	const name = getRndString();
 	// eslint-disable-next-line no-magic-numbers
@@ -13,6 +13,14 @@ describe('genInput', () => {
 		['name', name],
 		['age', age],
 	];
+	const mockActions = {
+		name: {
+			setName: jest.fn(),
+		},
+		age: {
+			setAge: jest.fn(),
+		},
+	};
 
 	test.each(expectations)('renders the component %p with appropriate value',
 		(param, paramValue) => {
@@ -28,4 +36,29 @@ describe('genInput', () => {
 			expect(component).toBeInTheDocument();
 			expect(component.value).toEqual(paramValue);
 		});
+
+	test.each(expectations)('required action for %p is triggered,'
+		+ 'when the input value is changed',
+	(param, paramValue) => {
+		const value = getRndString();
+		const actionNames = {
+			name: 'setName',
+			age: 'setAge',
+		};
+
+		const context = secure({
+			state: {
+				[param]: paramValue,
+			},
+			actions: mockActions[param],
+		});
+
+		const Result = genInput(param);
+		const component = render(Result(context)).getByRole(param);
+
+		fireEvent.change(component, { target: { value }});
+
+		expect(context.actions[actionNames[param]])
+			.toHaveBeenCalledWith(value);
+	});
 });
